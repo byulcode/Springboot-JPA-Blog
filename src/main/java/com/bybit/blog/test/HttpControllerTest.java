2bb0bd1307596e7340a0505ed69dc68e2162bef5
bybit0408 <bybit0408@gmail.com>
2021-11-20 18:18:04 +0900

    http connection

---
+++ src/main/java/com/bybit/blog/test/HttpControllerTest.java
@@ -0,0 +1,46 @@
+package com.bybit.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.experimental.PackagePrivate;
+//사용자가 요청 -> 응답(HTML 파일)
+//@Controller
+
+//사용자가 요청 -> 응답(Data)
+@RestController
+
+public class HttpControllerTest {
+	
+	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다!!!!
+	// http://localhost:8080/http/get		(select)
+	@GetMapping("/http/get")
+	public String getTest(Member m) {// MessageConverter(스프링부트)
+		// id=1&username=ssar&password=1234&email=ssar@naver.com
+		return "request get : " + m.getId()+", "+m.getUsername()+","+m.getPassword()+", "+m.getEmail();	
+	}
+	
+	// http://localhost:8080/http/post 		(insert)b
+	@PostMapping("/http/post") // text/plain, application/json
+	public String postTest(@RequestBody Member m) {// MessageConverter(스프링부트)
+		return "request post : "  + m.getId()+", "+m.getUsername()+","+m.getPassword()+", "+m.getEmail();	
+	}
+	
+	// http://localhost:8080/http/put		(update)
+	@PutMapping("/http/put")
+	public String putTest(@RequestBody Member m) {
+		return "request put"+ m.getId()+", "+m.getUsername()+","+m.getPassword()+", "+m.getEmail();	
+	}
+	
+	// http://localhost:8080/http/delete 	(delete)
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return "request delete";
+	}
+	
+}
