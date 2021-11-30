6c337ef13765550171ed0b1f18ad3dbbc632197c
bybit0408 <bybit0408@gmail.com>
2021-11-30 23:37:24 +0900

    create reply table

--- src/main/java/com/bybit/blog/model/Board.java
+++ src/main/java/com/bybit/blog/model/Board.java
@@ -1,19 +1,31 @@
 package com.bybit.blog.model;
 
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder  
 @Entity
 public class Board {
 	
@@ -30,9 +42,13 @@ public class Board {
 	@ColumnDefault("0")
 	private int count; 		//조회수
 	
-	@ManyToOne 	//Many = Board, One = User
+	@ManyToOne(fetch = FetchType.EAGER) 	//Many = Board, One = User
 	@JoinColumn(name="userId")
 	private User user;	//DB는 오브젝트를 저장할 수 없다. FK, 자바는 오브젝트를 저장할 수 있다
+	
+	//mappedBy 연관관계의 주인이 아니다(FK가 아니다) 그러니 DB에 컬럼을 만들지 마세요
+	@OneToMany(mappedBy = "board", fetch = FetchType.EAGER)		//이부분 다시정리(22강)
+	private List<Reply> reply;
 	
 	@CreationTimestamp
 	private Timestamp createDate;
--- src/main/java/com/bybit/blog/model/Reply.java
+++ src/main/java/com/bybit/blog/model/Reply.java
@@ -1,5 +1,47 @@
 package com.bybit.blog.model;
 
-public class Reply {
+import java.sql.Timestamp;
+import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder  
+@Entity
+public class Reply {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  // auto_increment
+	private int id;
+	
+	@Column(nullable=false, length=200)
+	private String content;
+	
+	@ManyToOne    //하나의 게시글에 여러 답변이 달릴 수 있다
+	@JoinColumn(name="boardId")
+	private Board board;
+	
+	@ManyToOne 		//하나의 유저가 여러개의 답변을 쓸 수 있다 
+	@JoinColumn(name="userId")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
 }
+
+
+
