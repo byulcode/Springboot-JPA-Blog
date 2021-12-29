2e5b2de4467afde9d2ce7b1fca155c770a702df0
bybit0408 <bybit0408@gmail.com>
2021-12-29 11:54:29 +0900

    login - traditional method

--- src/main/java/com/bybit/blog/repository/UserRepository.java
+++ src/main/java/com/bybit/blog/repository/UserRepository.java
@@ -11,7 +11,8 @@ import com.bybit.blog.model.User;
 //DAO
 //@자동으로 bean등록이 된다
 //@Repository //생략가능
-public interface UserRepository<T> extends JpaRepository<User, Integer>{
-
-
+public interface UserRepository extends JpaRepository<User, Integer>{
+	// JPA Naming 쿼리
+	// SELET * FROM user WHERE username = ? AND password = ?;
+	User findByUsernameAndPassword(String username, String password);
 }
